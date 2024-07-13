9b5ef95939cc340aa2002319ad9d9f84e8ae5855
CSWNI <descarlos2013@gmail.com>
2024-07-13 00:33:40 -0600

    chore: Update build workflow and remove .env.production file
    Updated the build workflow in API para ventas to include PHP setup and dotenv file deployment. Removed .env.production file from web para ventas. Also updated the update.sh script to change the service creation name from "npm" to "npm_INSTALL".

--- update.sh
+++ update.sh
@@ -5,11 +5,10 @@ cd /var/www/apps/ventas-api
 
 chmod +x deploy.sh
 
-# Crear el servicio usando la imagen de PHP 8.2 cswni/laravel-project:1.1.2
-docker service create --name npm --restart-condition none --mount type=bind,source=$(pwd),target=/usr/src/app node:lts /bin/bash -c 'cd /usr/src/app && npm install'
+docker service create --name npm_INSTALL --restart-condition none --mount type=bind,source=$(pwd),target=/usr/src/app node:lts /bin/bash -c 'cd /usr/src/app && npm install > /dev/null'
 
 # Esperar a que se complete el servicio
-SERVICE_NAME="npm"
+SERVICE_NAME="npm_INSTALL"
 # shellcheck disable=SC2046
 while [ $(docker service ps $SERVICE_NAME -f "desired-state=running" --no-trunc | wc -l) -gt 1 ]; do
     sleep 5
